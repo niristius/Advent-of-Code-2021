4d75448e432f072290a4f29375935db976e10a9e
Michel Hopfner <michel@hopfner.email>
2021-12-06 23:35:42 +0100

    be more efficient

--- day6/part1.py
+++ day6/part1.py
@@ -1,16 +1,26 @@
+import numpy as np
 input = open('input', 'r').read().split(',')
-listOfFish = input
 daysToSimulate = 80
+values, counts = np.unique(input, return_counts=True)
+FishMetrics = [[0, 0]]
+for i in range(len(values)):
+    CurrentValue = []
+    CurrentValue.append(int(values[i]))
+    CurrentValue.append(int(counts[i]))
+    FishMetrics.append(CurrentValue)
+for i in range(6, 9):
+    CurrentValue = []
+    CurrentValue.append(i)
+    CurrentValue.append(0)
+    FishMetrics.append(CurrentValue)
 
 for day in range (0, daysToSimulate):
-    newFishes = 0
-    for i in range(len(listOfFish)):
-        listOfFish[i] = int(listOfFish[i]) - 1
-        if listOfFish[i] == -1:
-            listOfFish[i] = 6
-            newFishes = newFishes +1
-
-    newFishes = [8] * newFishes
-    listOfFish.extend(newFishes)
-
-    print("Day " + str(day+1) + ": " + str(len(listOfFish)) + " fishes")
+    Fishamount = 0
+    FishesInLabour=FishMetrics[0][1]
+    for age in range(1, len(FishMetrics)):
+        FishMetrics[age - 1][1] = FishMetrics[age][1]
+    FishMetrics[8][1] = FishesInLabour
+    FishMetrics[6][1] = FishMetrics[6][1] + FishesInLabour
+    for i in range(len(FishMetrics)):
+        Fishamount = Fishamount + FishMetrics[i][1]
+    print("Day " + str(day+1) + ": " + str(Fishamount) + " fishes")
